e678ee327d9d2d1bf13aefa901e5b7f98c5a041a
WebDeveloper619 <webdevelopment619@gmail.com>
2024-10-14 01:32:33 +0500

    Capital and common Letter Added

--- src/app/shared-component/card-container/card-container.component.ts
+++ src/app/shared-component/card-container/card-container.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { cilMic, cilVolumeHigh } from '@coreui/icons';
+import { IconDirective } from '@coreui/icons-angular';
 
 @Component({
   selector: 'app-card-container',
+  standalone: true,
+  imports: [IconDirective],
   templateUrl: './card-container.component.html',
   styleUrl: './card-container.component.scss',
 })
-export class CardContainerComponent {}
+export class CardContainerComponent {
+  @Input() letter: string = '';
+  icons = { cilMic, cilVolumeHigh };
+}
