84c75a77d2101f445e7b68a283fcc7cb4bb9936f
WebDeveloper619 <webdevelopment619@gmail.com>
2024-10-13 23:59:02 +0500

    work

--- src/app/components/alphabets/alphabets.component.ts
+++ src/app/components/alphabets/alphabets.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from '../../home/header/header.component';
+import { IconDirective } from '@coreui/icons-angular';
+import { cilArrowLeft } from '@coreui/icons';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-alphabets',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, IconDirective],
   templateUrl: './alphabets.component.html',
-  styleUrl: './alphabets.component.scss'
+  styleUrl: './alphabets.component.scss',
 })
-export class AlphabetsComponent {
+export class AlphabetsComponent implements OnInit {
+  icons = { cilArrowLeft };
+  mode = '';
+  constructor(private router: Router, private activeRouter: ActivatedRoute) {}
 
+  ngOnInit(): void {
+    this.activeRouter.params.subscribe((params) => {
+      {
+        this.mode = params['mode'];
+      }
+    });
+  }
+
+  onChangePreviewMode(mode: string) {
+    this.router.navigate([`dashboard/${mode}`]);
+  }
 }
