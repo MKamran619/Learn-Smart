508ce2c1ac24d80d1ae8c6364c6d9a9dd12224c9
WebDeveloper619 <webdevelopment619@gmail.com>
2024-10-20 04:49:38 +0500

    updated

---
+++ src/app/components/pre-test/sentence-filter/sentence-filter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sentence-filter',
+  standalone: true,
+  imports: [],
+  templateUrl: './sentence-filter.component.html',
+  styleUrl: './sentence-filter.component.scss'
+})
+export class SentenceFilterComponent {
+
+}
