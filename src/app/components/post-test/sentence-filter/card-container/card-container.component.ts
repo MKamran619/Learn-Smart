3389e86f6eab1b3108fb972a295527bc4506dea0
WebDeveloper619 <webdevelopment619@gmail.com>
2024-10-21 03:21:13 +0500

    updated

---
+++ src/app/components/post-test/sentence-filter/card-container/card-container.component.ts
@@ -0,0 +1,136 @@
+import {
+  AfterViewInit,
+  Component,
+  Input,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+import {
+  cilMic,
+  cilVolumeHigh,
+  cilVolumeLow,
+  cilVolumeOff,
+} from '@coreui/icons';
+import { IconDirective, IconModule } from '@coreui/icons-angular';
+import { MatIcon } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { SpeechDetectService } from '../../../../services/speechDetect/speech-detect.service';
+
+@Component({
+  selector: 'app-card-container',
+  standalone: true,
+  imports: [CommonModule, IconDirective, IconModule, MatIcon],
+  templateUrl: './card-container.component.html',
+  styleUrl: './card-container.component.scss',
+})
+export class CardContainerComponent
+  implements OnInit, AfterViewInit, OnDestroy
+{
+  @Input() sentenceList: string[] = [];
+  sentence = '';
+  icons = { cilMic, cilVolumeHigh, cilVolumeLow, cilVolumeOff };
+  private intervalId: any;
+  totalLength = 0;
+  startFrom = 0;
+  showVolumIcon = false;
+
+  showCircle = false;
+  showQuaterCircle = true;
+  currentIndex = 0;
+  itemsCopy: any = [];
+
+  constructor(public accuracyService: SpeechDetectService) {}
+  ngOnInit(): void {
+    console.log('Component initialized');
+    this.startRandomSelection();
+  }
+  showQuaterCircleAnimation() {
+    this.showQuaterCircle = true;
+    setTimeout(() => {
+      this.showQuaterCircle = false;
+    }, 1500);
+  }
+  showCircleAnimation() {
+    this.showCircle = false;
+    setTimeout(() => {
+      this.showCircle = true;
+    }, 50);
+  }
+  ngAfterViewInit(): void {
+    // this.startRandomSelection();
+  }
+  startRandomSelection() {
+    this.accuracyService.resultList = [];
+    this.accuracyService.transcription = '';
+    this.accuracyService.accuracyScore = 0;
+    this.totalLength = this.sentenceList.length;
+
+    // Create a copy of the original array and shuffle it
+    this.itemsCopy = [...this.sentenceList];
+    // this.shuffleArray(itemsCopy);
+
+    this.currentIndex = 0;
+
+    this.onCallAccuracyFunction();
+
+    this.intervalId = setInterval(() => {
+      this.onCallAccuracyFunction();
+    }, 10000);
+  }
+
+  onCallAccuracyFunction() {
+    this.showCircleAnimation();
+    this.showQuaterCircleAnimation();
+
+    if (this.sentence) {
+      const result = {
+        sentence: this.sentence,
+        noResponse: this.accuracyService.transcription ? '' : 'noResponse',
+        userSpoke: this.accuracyService.transcription,
+        accuracy: this.accuracyService.accuracyScore.toFixed(0),
+      };
+
+      this.accuracyService.resultList.push(result);
+    }
+    if (this.currentIndex >= this.itemsCopy.length) {
+      this.clearInterval();
+      console.log('result = ', this.accuracyService.resultList);
+      this.accuracyService.onShowResult = true;
+      console.log('All items have been selected. Stopping random selection.');
+      return;
+    }
+    this.startFrom = this.startFrom + 1;
+    this.sentence = this.itemsCopy[this.currentIndex];
+    this.accuracyService.referenceText = `${this.sentence}`;
+    if (this.showVolumIcon) {
+      this.showQuaterCircle = true;
+      // this.showQuaterCircleAnimation();
+      this.accuracyService.speakText(this.accuracyService.referenceText);
+    }
+
+    this.accuracyService.startSpeechRecognition();
+
+    this.currentIndex++;
+  }
+
+  private shuffleArray(array: string[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+  clearInterval() {
+    // Clear the interval if it exists
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+      this.intervalId = null; // Reset the intervalId to null
+    }
+  }
+  ngOnDestroy() {
+    // Clear interval on component destruction to prevent memory leaks
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+}
--- src/app/components/post-test/sentence-filter/sentence-filter.component.ts
+++ src/app/components/post-test/sentence-filter/sentence-filter.component.ts
@@ -1,11 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { HeaderComponent } from '../../../home/header/header.component';
+import { IconDirective } from '@coreui/icons-angular';
+import { cilArrowLeft } from '@coreui/icons';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CardContainerComponent } from './card-container/card-container.component';
+import { SpeechDetectService } from '../../../services/speechDetect/speech-detect.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ResultTableComponent } from './result-table/result-table.component';
 
 @Component({
-  selector: 'app-post-sentence-filter',
+  selector: 'app-sentence-filter',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [
+    HeaderComponent,
+    CardContainerComponent,
+    IconDirective,
+    FormsModule,
+    CommonModule,
+    ResultTableComponent,
+  ],
   templateUrl: './sentence-filter.component.html',
   styleUrl: './sentence-filter.component.scss',
 })
-export class SentenceFilterComponent {}
+export class SentenceFilterComponent implements OnInit {
+  @ViewChild(CardContainerComponent) childComponent!: CardContainerComponent;
+
+  icons = { cilArrowLeft };
+  mode = '';
+  btnCapitalActive = true;
+  selectedTab = 'Capital Letter';
+  sentenceList: string[] = [];
+  constructor(
+    private router: Router,
+    private activeRouter: ActivatedRoute,
+    public accuracyService: SpeechDetectService
+  ) {}
+
+  ngOnInit(): void {
+    this.activeRouter.params.subscribe((params) => {
+      {
+        this.mode = params['mode'];
+      }
+    });
+    this.onLoadInitialData();
+  }
+  onLoadInitialData() {
+    this.accuracyService.resultList = [];
+    this.accuracyService.accuracyScore = 0;
+    this.accuracyService.transcription = '';
+    this.btnCapitalActive = true;
+    this.sentenceList = [
+      'I can play with the bat and ball here.',
+      'The three boys like to walk to the bus stop.',
+      'We are sleeping. We woke up late and we are very tired.',
+      'Mother and father work from home. They help people and tell them what to do. Some of them left their houses very early.',
+      'Sometimes we need to place animals into groups of same and different. Together, we must write the important ones on the same list.',
+    ];
+  }
+
+  onChangePreviewMode(mode: string) {
+    if (this.accuracyService.onShowResult) {
+      this.accuracyService.onShowResult = false;
+    } else {
+      this.router.navigate([`dashboard/${mode}`]);
+    }
+  }
+}
